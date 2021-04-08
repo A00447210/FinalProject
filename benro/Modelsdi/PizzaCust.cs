dcc85c1bf8b90ae0c7c18a120fec0e3ab3939a0c
pragatichaturvedi1992 <77502679+pragatichaturvedi1992@users.noreply.github.com>
2021-04-08 19:15:18 +0530

    scaffolding

---
+++ benro/Modelsdi/PizzaCust.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace PizzaKnight.Modelsdi
+{
+    public partial class PizzaCust
+    {
+        public PizzaCust()
+        {
+            OrderDetail = new HashSet<OrderDetail>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoriesId { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetail { get; set; }
+    }
+}
